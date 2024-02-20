e293ac5d0153b8017f8b00d0320358dee678c19b
Hannah Thompson <hannahmthompsonedu@gmail.com>
2024-02-20 13:48:35 -0500

    pushing SQL

---
+++ SQL/Bookshelf_Create_DB.sql
@@ -0,0 +1,94 @@
+﻿USE [master]
+
+IF db_id('Bookshelf') IS NULL
+  CREATE DATABASE [Bookshelf]
+GO
+
+USE [Bookshelf]
+GO
+
+DROP TABLE IF EXISTS [User];
+DROP TABLE IF EXISTS [Books];
+DROP TABLE IF EXISTS [Author];
+DROP TABLE IF EXISTS [Genre];
+DROP TABLE IF EXISTS [bookClub];
+DROP TABLE IF EXISTS [Follow];
+DROP TABLE IF EXISTS [Message];
+GO
+
+CREATE TABLE [User] (
+  [id] int PRIMARY KEY IDENTITY,
+  [userName] nvarchar(255),
+  [password] nvarchar(255),
+  [firstName] nvarchar(255),
+  [lastName] nvarchar(255),
+  [imageURL] nvarchar(255)
+)
+GO
+
+CREATE TABLE [Genre] (
+  [id] int PRIMARY KEY IDENTITY,
+  [name] nvarchar(255)
+)
+GO
+
+CREATE TABLE [Author] (
+  [id] int PRIMARY KEY IDENTITY,
+  [name] nvarchar(255)
+)
+GO
+
+CREATE TABLE [Books] (
+  [id] int PRIMARY KEY IDENTITY,
+  [userId] int,
+  [title] nvarchar(255),
+  [currentPage] nvarchar(255),
+  [totalPage] int,
+  [genreId] int,
+  [authorId] int,
+)
+GO
+
+CREATE TABLE [bookClub] (
+  [id] int PRIMARY KEY IDENTITY,
+  [userId] int,
+  [bookId] int,
+)
+GO
+
+CREATE TABLE [Follow] (
+  [id] int PRIMARY KEY IDENTITY,
+  [userId] int,
+  [friendId] int
+)
+GO
+
+CREATE TABLE [Message] (
+  [id] int PRIMARY KEY IDENTITY,
+  [userId] int,
+  [bookClubId] int,
+  [content] nvarchar(255),
+  [sendDateTime] date
+)
+GO
+
+ALTER TABLE [Books] ADD FOREIGN KEY ([userId]) REFERENCES [User] ([id])
+GO
+
+ALTER TABLE [Books] ADD FOREIGN KEY ([authorId]) REFERENCES [Author] ([id])
+GO
+
+ALTER TABLE [bookClub] ADD FOREIGN KEY ([userId]) REFERENCES [User] ([id])
+GO
+
+ALTER TABLE [Books] ADD FOREIGN KEY ([genreId]) REFERENCES [Genre] ([id])
+GO
+
+ALTER TABLE [Follow] ADD FOREIGN KEY ([userId]) REFERENCES [User] ([id])
+GO
+
+ALTER TABLE [Message] ADD FOREIGN KEY ([userId]) REFERENCES [User] ([id])
+GO
+
+ALTER TABLE [Message] ADD FOREIGN KEY ([bookClubId]) REFERENCES [bookClub] ([id])
+GO
---
+++ SQL/Bookshelf_Seed_Data.sql
@@ -0,0 +1,45 @@
+﻿USE [Bookshelf];
+GO
+
+set identity_insert [User] on
+insert into [User] (id, firstName, lastName, userName, imageUrl, password) values (1, 'Gloriana', 'Morfield', 'gmorfield0', 'https://robohash.org/utaperiamneque.png?size=50x50&set=set1', 'pass');
+insert into [User] (id, firstName, lastName, userName, imageUrl, password) values (2, 'Gasper', 'Manlow', 'gmanlow1', 'https://robohash.org/voluptatibusdolorrerum.png?size=50x50&set=set1', 'pass1');
+insert into [User](id, firstName, lastName, userName, imageUrl, password) values (3, 'Fifine', 'Suffield', 'fsuffield2', 'https://robohash.org/etaspernaturipsum.png?size=50x50&set=set1', 'pass2');
+set identity_insert [User] off
+
+set identity_insert [Genre] on
+insert into Genre (id, name) values (1, 'Fiction');
+insert into Genre (id, name) values (2, 'Non-Fiction');
+insert into Genre (id, name) values (3, 'Self-Help');
+insert into Genre (id, name) values (4, 'Mystery');
+insert into Genre (id, name) values (5, 'Adventure');
+set identity_insert [Genre] off
+
+set identity_insert [Author] on
+insert into Author (id, name) values (1, 'Sarah J. Maas');
+insert into Author (id, name) values (2, 'Madeline Miller');
+set identity_insert [Author] off
+
+set identity_insert [Books] on
+insert into Books (id, userId, title, currentPage, totalPage, genreId, authorId) values (1, 1, 'Throne of Glass', 24, 388, 1, 1);
+insert into Books (id, userId, title, currentPage, totalPage, genreId, authorId) values (2, 2, 'Song of Achilles', 63, 258, 2, 2);
+insert into Books (id, userId, title, currentPage, totalPage, genreId, authorId) values (3, 3, 'Crescent City', 134, 419, 1, 1);
+set identity_insert [Books] off
+
+set identity_insert [bookClub] on
+insert into bookClub (id, userId, bookId) values (1, 1, 1);
+insert into bookClub (id, userId, bookId) values (2, 2, 2);
+insert into bookClub (id, userId, bookId) values (3, 3, 3);
+set identity_insert [bookClub] off
+
+set identity_insert [Follow] on
+insert into Follow (id, userId, friendId) values (1, 1, 1);
+insert into Follow (id, userId, friendId) values (2, 2, 2);
+insert into Follow (id, userId, friendId) values (3, 3, 3);
+set identity_insert [Follow] off
+
+set identity_insert [Message] on
+insert into Message (id, userId, bookClubId, content, sendDateTime) values (1, 1, 1, 'envisioneer next-generation initiatives', '7/19/2023');
+insert into Message (id, userId, bookClubId, content, sendDateTime) values (2, 2, 2, 'orchestrate integrated initiatives', '4/25/2023');
+insert into Message (id, userId, bookClubId, content, sendDateTime) values (3, 3, 3, 'synergize vertical e-business', '5/31/2023');
+set identity_insert [Message] off
